c50ae8298f799acff2747d0406d01ea92d349f69
polinaproo <153515314+polinaproo@users.noreply.github.com>
2023-12-12 21:59:38 +0400

    Update pr12no4.c

--- pr12no4.c
+++ pr12no4.c
@@ -7,10 +7,13 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	char* str1, * str2;
+	printf("Введите первую строку:\n ");
 	str1 = (char*)calloc(1, sizeof(char));
 	gets(str1);
+	printf("Введите вторую строку: \n");
 	str2 = (char*)calloc(1, sizeof(char));
 	gets(str2);
+	printf("Преобразованная строка: \n");
 	for (int i = 0; i < strlen(str1); i++)
 	{
 		printf("%c", str1[i]);
